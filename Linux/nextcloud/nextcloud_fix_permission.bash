882c886355ea1d18c2199131e1268d3f90b70cbe
kerus1024 <kerus1024@gmail.com>
2020-06-13 01:47:56 +0900

    적용되지 않던거 수장

--- Linux/nextcloud/nextcloud_fix_permission.bash
+++ Linux/nextcloud/nextcloud_fix_permission.bash
@@ -5,8 +5,13 @@ DAEMON_USER="nginx"
 DAEMON_GROUP="nginx"
 PHP_PATH="/usr/bin/php"
 
-chown -R ${DAEMON_USER}:${DAEMON_GROUP} $BASE_PATH
-find $DATA_PATH -type f -exec chmod 0660 {} \;
-find $DATA_PATH -type d -exec chmod 0770 {} \;
+#chown -R ${DAEMON_USER}:${DAEMON_GROUP} $BASE_PATH;
+chown -R ${DAEMON_USER}:${DAEMON_GROUP} $DATA_PATH;
+
+#find $BASE_PATH -type f -exec chmod 0644 {} \;
+#find $BASE_PATH -type d -exec chmod 0755 {} \;
+
+find $DATA_PATH -type f -exec chmod 0644 {} \;
+find $DATA_PATH -type d -exec chmod 0775 {} \;
 
 sudo -u ${DAEMON_USER} $PHP_PATH ${BASE_PATH}/occ files:scan --all
